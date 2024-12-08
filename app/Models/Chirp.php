1ce1e91c5d4f1e2dfaeb46128752da76aa2f48d9
Kanelone4 <chicoufarell05@gmail.com>
2024-12-08 18:15:38 +0100

    afficher chirps

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,11 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Chirp extends Model
 {
+
+    use HasFactory;
     protected $fillable = [
         'message',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
